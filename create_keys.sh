7c0ab369dffce3746d7f2b4265fb4a04ecf1adac
Nicolas Viennot <nicolas@viennot.biz>
2019-07-31 20:55:04 -0400

    Show tmate.conf settings when generating keys

--- create_keys.sh
+++ create_keys.sh
@@ -1,13 +1,27 @@
 #!/bin/bash
+set -eu
+
 gen_key() {
   keytype=$1
   ks="${keytype}_"
   key="keys/ssh_host_${ks}key"
   if [ ! -e "${key}" ] ; then
     ssh-keygen -t ${keytype} -f "${key}" -N ''
-    return $?
+    SIG=$(ssh-keygen -l -E SHA256 -f $key.pub | cut -d ' ' -f 2)
   fi
 }
 
 mkdir -p keys
-gen_key rsa && gen_key ed25519 || exit 1
+gen_key rsa
+RSA_SIG=$SIG
+gen_key ed25519
+ED25519_SIG=$SIG
+
+
+echo ""
+echo "You may use the following settings this in your .tmate.conf:"
+echo ""
+echo "set -g tmate-server-host localhost"
+echo "set -g tmate-server-port 22"
+echo "set -g tmate-server-rsa-fingerprint \"$RSA_SIG\""
+echo "set -g tmate-server-ed25519-fingerprint \"$ED25519_SIG\""
